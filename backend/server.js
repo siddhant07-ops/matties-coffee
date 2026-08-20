72983ccbfa8e725a329ce6673c2d0dc0198ba8f2
Siddhant Nalla <nallasiddhantp@gmail.com>
2026-08-20 22:14:07 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ const startServer = async () => {
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log(
-      console.log(`✅ Server is running on port ${PORT}`);
+      `Server is running on port ${PORT}`
     );
   });
 };
