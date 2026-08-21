3b5dcbb3b9d9ac24e3a0acbaba1458a3285dde51
Siddhant Nalla <nallasiddhantp@gmail.com>
2026-08-21 20:51:30 +0530

    Debug production API URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 const API_BASE_URL =
   import.meta.env.VITE_API_URL || "http://localhost:5000";
-console.log("API BASE URL:", API_BASE_URL);
+  
+  console.log("API BASE URL:", API_BASE_URL);
 export const apiRequest = async (
   endpoint,
   options = {}
